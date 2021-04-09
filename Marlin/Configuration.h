5e46b58ea48cc2d335eb05476cf0e1ca98f4deea
Ondřej Hanousek <ondrej.hanousek@ctgroup.cz>
2021-04-09 09:01:07 +0200

    units fixed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -741,7 +741,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200.9, 201, 200.9, 100, 100 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 200, 100, 100 }
 
 /**
  * Default Max Feed Rate (mm/s)
