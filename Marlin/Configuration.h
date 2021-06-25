977ad2bbab567772ac72be3110212e53dcce885f
Ondřej Hanousek <ondrej.hanousek@ctgroup.cz>
2021-06-25 11:35:26 +0200

    Bed resized.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1122,8 +1122,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 280
+#define X_BED_SIZE 250
+#define Y_BED_SIZE 310
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
